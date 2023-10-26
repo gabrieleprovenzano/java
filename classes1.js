29f6c46abac20093c8fef352a55d34392854d33f
gabrieleprovenzano <146100399+gabrieleprovenzano@users.noreply.github.com>
2023-10-26 17:22:45 +0200

    Add files via upload

---
+++ classes1.js
@@ -0,0 +1,9 @@
+class Person { 
+    constructor (firstname , lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+}
+
+let gabriele = new Person ("Gabriele" , "Provenzano");
+console.log(gabriele);
