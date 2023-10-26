61db9037a8926565e087fb81bbe0e2f782471c88
gabrieleprovenzano <146100399+gabrieleprovenzano@users.noreply.github.com>
2023-10-26 17:45:18 +0200

    Add files via upload

--- classes1.js
+++ classes1.js
@@ -1,9 +1,19 @@
-class Person { 
-    constructor (firstname , lastname) {
-        this.firstname = firstname;
-        this.lastname = lastname;
+class Person {
+    constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
     }
 }
 
-let gabriele = new Person ("Gabriele" , "Provenzano");
+class developer extends Person{
+    constructor(firstName , lastName , role){
+        super(firstName , lastName);
+        this.role = role;
+    }
+    
+}
+    
+const mario = new developer("Mario", "Rossi", "Front-end");
+const gabriele = new Person("Gabriele" , "Provenzano" , "Full-stack");
 console.log(gabriele);
+console.log(mario);
